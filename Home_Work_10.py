055d13a886ad03c16d3fa4741fc648647011ce0a
limboodo <limboodo.icloud.com>
2021-11-14 14:44:18 +0200

    Home_Work_10

---
+++ Home_Work_10.py
@@ -0,0 +1,6 @@
+set_1 = {1, 2, 3, 4}
+set_2 = {2, 3, 4, 5}
+if set_1 >= set_2:
+    print(True)
+else:
+    print(False)
--- Home_Work_9.py
+++ Home_Work_9.py
@@ -1,8 +1,8 @@
 a = {}
-for word in input().split():
-    if word in a:
-        a[word] = a.get(word) + 1
-        print(a.get(word))
+for text in input().split():
+    if text in a:
+        a[text] = a.get(text) + 1
+        print(a.get(text))
     else:
-        a[word] = 1
-        print(a.get(word))
+        a[text] = 1
+        print(a.get(text))
